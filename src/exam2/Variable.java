8ba0e65d91d8ddb6d1dbff6fd6da4994c68a879e
choi <remember0762@gmail.com>
2024-08-02 20:48:13 +0900

    변수연습

---
+++ src/exam2/Variable.java
@@ -0,0 +1,12 @@
+package exam2;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a;
+        a = 1;
+        System.out.println(a + 1);
+
+        a = 2;
+        System.out.println(a + 1);
+    }
+}
