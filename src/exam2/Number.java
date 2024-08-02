a2d3f3156f6040baf55b502157c3fdeb7d1d8e4f
choi <remember0762@gmail.com>
2024-08-02 20:17:20 +0900

    숫자연산해보기

---
+++ src/exam2/Number.java
@@ -0,0 +1,10 @@
+package exam2;
+
+public class Number {
+    public static void main(String[] args) {
+        System.out.println(1 +1 );
+        System.out.println(1.2 + 1.3);
+        System.out.println(2 * 5);
+        System.out.println(6 / 2);
+    }
+}
