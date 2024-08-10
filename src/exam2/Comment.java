31320b5e3c1ec6db138433dd1cc9e16f887d3c31
choi <remember0762@gmail.com>
2024-08-10 18:03:43 +0900

    주석작성해보기
    (cherry picked from commit ae9865b449c4bc93a4530723c209c6586f5f5240)

---
+++ src/exam2/Comment.java
@@ -0,0 +1,13 @@
+package exam2;
+
+public class Comment {
+    public static void main(String[] args) {
+        // 두개의 변수가 같은 데이터 타입 일 때 아래와 같이 코드를 작성한다.
+        String a, b;
+        /*
+        a = "coding";
+        b = "everybody";
+        System.out.println(a+b);
+         */
+    }
+}
